0da6fc96685f6fbb13a7e2fb87a53c0fa27eab1a
Tajim Islam <tjm4181@gmail.com>
2024-03-21 01:23:41 +0600

    'vendo_add'

--- resources/views/fronted/team.blade.php
+++ resources/views/fronted/team.blade.php
@@ -13,27 +13,24 @@
         </div>
     </div>
 <!-- Navbar End -->
-
-
-        <!-- team Start -->
-        <div class="container-fluid py-5 ">
-            <div class="container pt-2">
-                
-                <div class="row g-5 single_team mt-2">
-                    <div class=" col-lg-6 wow fadeIn" style="margin: 0px auto;">
-                        <div class="member d-flex align-items-start" >
-                            <div class="pic">
-                                <img alt="" class="img-fluid" src="{{ asset('') }}fronted/assets/img/team-2.jpg">
-                            </div> 
-                            <div class="member-info">
-                                <h4 class="text-uppercase">Md. Abdul Mannan Sourov</h4> <span>Project Manager</span> 
-                                <p>10 Years Experiences.</p> 
-                                <div class="social">
-                                    <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-facebook"></i></a> 
-                                    <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-twitter"></i></a> 
-                                    <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-instagram"></i></a> 
-                                    <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-linkedin"></i></a></div></div>
-                                </div>
+<!-- team Start -->
+<div class="container-fluid py-5 ">
+    <div class="container pt-2">
+        <div class="row g-5 single_team mt-2">
+            <div class=" col-lg-6 wow fadeIn" style="margin: 0px auto;">
+                <div class="member d-flex align-items-start" >
+                    <div class="pic">
+                        <img alt="" class="img-fluid" src="{{ asset('') }}fronted/assets/img/team-2.jpg">
+                    </div> 
+                    <div class="member-info">
+                        <h4 class="text-uppercase">Md. Abdul Mannan Sourov</h4> <span>Project Manager</span> 
+                        <p>14 Years Experiences.</p> 
+                        <div class="social">
+                            <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-facebook"></i></a> 
+                            <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-twitter"></i></a> 
+                            <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-instagram"></i></a> 
+                            <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-linkedin"></i></a></div></div>
+                        </div>
                     </div>
                 </div>
                 <div class="row  wow fadeIn ">
@@ -43,8 +40,8 @@
                                 <img alt="" class="img-fluid" src="{{ asset('') }}fronted/assets/img/team-2.jpg">
                             </div> 
                             <div class="member-info">
-                                <h4 class="text-uppercase">Md. Abdul Mannan Sourov</h4> <span>Project Manager</span> 
-                                <p>10 Years Experiences.</p> 
+                                <h4 class="text-uppercase">Manjur Ajad</h4> <span>Project Manager</span> 
+                                <p>6 Years Experiences.</p> 
                                 <div class="social">
                                     <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-facebook"></i></a> 
                                     <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-twitter"></i></a> 
@@ -58,8 +55,8 @@
                                 <img alt="" class="img-fluid" src="{{ asset('') }}fronted/assets/img/team-2.jpg">
                             </div> 
                             <div class="member-info">
-                                <h4 class="text-uppercase">Md. Abdul Mannan Sourov</h4> <span>Project Manager</span> 
-                                <p>10 Years Experiences.</p> 
+                                <h4 class="text-uppercase">Samsul Karim Tanim</h4> <span>Senior Developer</span> 
+                                <p>4 Years Experiences.</p> 
                                 <div class="social">
                                     <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-facebook"></i></a> 
                                     <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-twitter"></i></a> 
@@ -73,8 +70,8 @@
                                 <img alt="" class="img-fluid" src="{{ asset('') }}fronted/assets/img/team-2.jpg">
                             </div> 
                             <div class="member-info">
-                                <h4 class="text-uppercase">Md. Abdul Mannan Sourov</h4> <span>Project Manager</span> 
-                                <p>10 Years Experiences.</p> 
+                                <h4 class="text-uppercase">Rifat Rabby</h4> <span>Java Application Developer</span> 
+                                <p>7 Years Experiences.</p> 
                                 <div class="social">
                                     <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-facebook"></i></a> 
                                     <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-twitter"></i></a> 
@@ -88,8 +85,8 @@
                                 <img alt="" class="img-fluid" src="{{ asset('') }}fronted/assets/img/team-2.jpg">
                             </div> 
                             <div class="member-info">
-                                <h4 class="text-uppercase">Md. Abdul Mannan Sourov</h4> <span>Project Manager</span> 
-                                <p>10 Years Experiences.</p> 
+                                <h4 class="text-uppercase">Mobinul Islam Tazim</h4> <span>PHP/Laravel Developer</span> 
+                                <p>4 Years Experiences.</p> 
                                 <div class="social">
                                     <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-facebook"></i></a> 
                                     <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-twitter"></i></a> 
@@ -103,8 +100,8 @@
                                 <img alt="" class="img-fluid" src="{{ asset('') }}fronted/assets/img/team-2.jpg">
                             </div> 
                             <div class="member-info">
-                                <h4 class="text-uppercase">Md. Abdul Mannan Sourov</h4> <span>Project Manager</span> 
-                                <p>10 Years Experiences.</p> 
+                                <h4 class="text-uppercase">Mohammad Ala Uddin</h4> <span>PHP/Laravel Developer</span> 
+                                <p>4 Years Experiences.</p> 
                                 <div class="social">
                                     <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-facebook"></i></a> 
                                     <a target="_blank" href="https://www.facebook.com/sourov.uiu"><i class="fab fa-twitter"></i></a> 
